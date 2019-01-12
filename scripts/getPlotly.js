945e6e9a3cfb13a733ef3e68054b1be534817675
Ryan Baxley <ryan.baxley1@gmail.com>
2019-01-12 13:52:21 -0500

    Update get plotly script

---
+++ scripts/getPlotly.js
@@ -0,0 +1,43 @@
+const https = require('https');
+const fs = require('fs');
+
+function downloadPlotly(path, name) {
+  return new Promise((resolve, reject) => {
+    https
+      .get(`https://cdn.plot.ly/${path}`, resp => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', chunk => {
+          data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+          const b64Data = Buffer.from(data).toString('base64');
+          const fileContent = `const content = \`${b64Data}\`;\nexport default content;\n`;
+          fs.writeFile(`src/lib/${name}.ts`, fileContent, err => {
+            if (err) reject(err);
+            else resolve();
+          });
+        });
+      })
+      .on('error', err => {
+        reject(err);
+      });
+  });
+}
+
+function main() {
+  return Promise.all([
+    downloadPlotly('plotly-basic-latest.min.js', 'PlotlyBasic'),
+    downloadPlotly('plotly-latest.min.js', 'PlotlyFull'),
+  ]);
+}
+
+main()
+  .then(() => console.log('Finished!'))
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
