28e3deb683804ce8d26d0e2978d4a2a38ef3d648
Ryan Baxley <ryan.baxley1@gmail.com>
2019-12-10 18:44:48 -0500

    Refactor app to function component

--- examples/basic/App.js
+++ examples/basic/App.js
@@ -1,58 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Button } from 'react-native';
 import Plotly from 'react-native-plotly';
 
-const upData = {
+const upTrace = {
   __id: 'up',
   x: [1, 2, 3, 4, 5],
   y: [1, 2, 3, 4, 8],
   type: 'scatter',
 };
 
-const downData = {
+const downTrace = {
   __id: 'down',
   x: [1, 2, 3, 4, 5],
   y: [8, 4, 3, 2, 1],
   type: 'scatter',
 };
 
-export default class App extends React.Component {
-  state = {
-    data: [upData],
-    layout: { title: 'Plotly.js running in React Native!' },
-  };
+const App = () => {
+  const [trace, setTrace] = useState(upTrace);
 
-  swapData = () => {
-    if (this.state.data[0].__id === 'up') {
-      this.setState({ data: [downData] });
+  function swapData() {
+    if (trace.__id === 'up') {
+      setTrace(downTrace);
     } else {
-      this.setState({ data: [upData] });
+      setTrace(upTrace);
     }
-  };
+  }
 
-  update = (_, { data, layout, config }, plotly) => {
+  const update = (_, { data, layout, config }, plotly) => {
     plotly.react(data, layout, config);
   };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.buttonRow}>
-          <Button onPress={this.swapData} title="Swap Data" />
-        </View>
-        <View style={styles.chartRow}>
-          <Plotly
-            data={this.state.data}
-            layout={this.state.layout}
-            update={this.update}
-            onLoad={() => console.log('loaded')}
-            debug
-          />
-        </View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonRow}>
+        <Button onPress={swapData} title="Swap Data" />
       </View>
-    );
-  }
-}
+      <View style={styles.chartRow}>
+        <Plotly
+          data={[trace]}
+          layout={{ title: 'Plotly.js running in React Native!' }}
+          update={update}
+          onLoad={() => console.log('loaded')}
+          debug
+        />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   buttonRow: {
@@ -71,3 +66,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
